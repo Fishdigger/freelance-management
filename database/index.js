9c79d1773eb2019bc3c60ed9ff58ab1ff007b410
Joel Gregory <joelgregory7@gmail.com>
2017-12-27 15:30:04 -0500

    Removed unecessary comments

--- database/index.js
+++ database/index.js
@@ -5,18 +5,6 @@ const url = process.env.DB_URL;
 const auth = {auth: {user: process.env.DB_USER, password: process.env.DB_PASS}};
 const db_name = "freelance-management";
 
-// module.exports.get = (query, collection) => {
-//     return mongoClient.connect(url, auth).then((client) => {
-//         return client.db(db_name).collection(collection).find(query).toArray();
-//     }).then((client) => {
-//         client.close();
-//     });
-// }
-
-// module.exports.get({}, "Clients").then((data) => {
-//     console.log(data);
-// });
-
 let db;
 
 module.exports.connect = () => {
@@ -25,15 +13,3 @@ module.exports.connect = () => {
         return db;
     })
 }
-
-// mongoClient.connect(url, {auth: {user: user, password: pass}}, (err, client) => {
-//     if (err) throw err;
-//     console.log("Successfully connected to DB");
-//     let db = client.db("freelance-management")
-//     db.collection("Clients").find({}).toArray((err, data) => {
-//         if (err) throw err;
-//         console.log(data);
-//         client.close();
-//     });
-// });
-
